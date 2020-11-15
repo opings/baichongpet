8ac5df1a85d3e3d5bbd6687f85423f4b55be692d
zhaoyongzhen <zhaoyongzhen@hupu.com>
2020-11-15 13:59:53 +0800

    excel 导入数据

---
+++ src/main/java/com/baichong/exceldemo/ArticleExcelData.java
@@ -0,0 +1,14 @@
+package com.baichong.exceldemo;
+
+import lombok.Data;
+
+@Data
+public class ArticleExcelData {
+    private String string1;
+    private String string2;
+    private String string3;
+    private String string4;
+    private String string5;
+    private String string6;
+    private String string7;
+}
